fc30f76b14ab8f7ca554b383efdfe4dbf702f44a
gyuseon <62982553+gyuseon@users.noreply.github.com>
2020-04-13 15:56:54 +0900

    Update MemberTable.java

--- src/table/MemberTable.java
+++ src/table/MemberTable.java
@@ -254,7 +254,9 @@ public class MemberTable extends JFrame implements ActionListener{
 				JOptionPane.showMessageDialog(this, "삭제실패");
 		}
 		
-	}
+	}else if(e.getActionCommand().equals("수정")){
+		System.out.println("원격저장소");
+		}
 	}
 }
 
